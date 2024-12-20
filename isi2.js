3a3f9db0c2f0a337742c76f0a97ef706b681b47b
najwa-byte <najwakurnia31@gmail.com>
2024-12-20 15:59:51 +0700

    Add files via upload

---
+++ isi2.js
@@ -0,0 +1,25 @@
+document.write("<hr width=110% color=red>");
+document.writeln("<h1 align=center>WEBSITE Profile <font color=pink> Najwa Kurnia Ningtiyas Rusli</font></h1>");
+document.write("<hr width=110% color=red>");
+document.writeln("<div align=center> <img src=profile.jpg width=10% height=10% alt=foto saya></div>");
+
+document.writeln("<h2>Biodata : </h2>");
+document.writeln("<ol type=1 style='font-size:smaller ; font-weight:normal;>");
+document.writeln("<li> Nim : 241011700507 </li>");
+document.writeln("<li> Nama : Najwa Kurnia Ningtiyas Rusli </li>");
+document.writeln("<li> Kelas : 01SIFP016 </li>");
+document.writeln("</ol>");
+document.writeln("<ul> type=circle style='font-size:smaller ; font-weight:normal;'>")
+document.writeln("<li> Program Studi Sistem Informasi </li>")
+document.writeln("<li> Alamat : Jl Berlian IV, Perum Graha Idaman 15 Blok D15 RT 004 RW 005, Kec Gunung sindur , Kab Bogor </li>")
+document.writeln("<li> Asal Sekolah : SMA NEGERI 5 MEDAN </li>")
+document.writeln("</ul>")
+document.writeln("<ol type=i style='font-size:smaller ; font-weight:normal;'>")
+document.writeln("<li> Jurusan : IPA </li>")
+document.writeln("<li> Email : najwakurnia31@gmail.com </li>")
+document.writeln("<li> Telepon : 0895-1277-5806 </li>")
+document.writeln("</ol>")
+
+document.writeln("<hr width=110% color=red>");
+document.writeln("<p align=center>Design by Najwa Kurnia Nigtiyas Rusli &copy; 2024 AllRight Reserved</p>")
+document.writeln("<hr width=110% color=red>");
